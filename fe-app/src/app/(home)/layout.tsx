0b3d13d93d873e7b891b6f6075ad29b1647b8899
lawrient <sandincl.d@gmail.com>
2025-07-16 05:52:14 +0800

    auth get data current

--- fe-app/src/app/(home)/layout.tsx
+++ fe-app/src/app/(home)/layout.tsx
@@ -9,6 +9,18 @@ export default async function Layout(props: { children: React.ReactNode }) {
   const cookie = await cookies();
 
   if (!cookie.get("token")) redirect("/login");
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  fetch(`${process.env.NEXT_PUBLIC_HOST_API}auth/me`, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + cookie.get("token")?.value,
+    },
+  }).then(async (res) => {
+    const resp = await res.json();
+    console.log(resp);
+  });
+
   return (
     <div className="fixed grid h-screen w-screen grid-rows-[auto_1fr] bg-neutral-950 text-white">
       <Header />
