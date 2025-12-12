e709a1fa38203aa82b253a89d060315f29e89da5
lawrient <sandincl.d@gmail.com>
2025-12-12 18:24:20 +0800

    fix phising attribute

--- fe-app/src/app/(auth)/layout.tsx
+++ fe-app/src/app/(auth)/layout.tsx
@@ -11,14 +11,6 @@ export default async function Layout(props: { children: React.ReactNode }) {
       style={{ backgroundImage: "url('/bg.svg')", backgroundSize: "cover" }}
       className="fixed flex h-screen w-screen items-center justify-center"
     >
-      <Image
-        className="absolute top-0 left-0 m-11"
-        src="/discord.svg"
-        width={124}
-        height={24}
-        alt="logo discord name"
-        priority
-      />
       {props.children}
     </div>
   );
